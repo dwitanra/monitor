7ee7524de7f5f6fdc62309dacace91fa45bc1845
den85nis <den85nis@yahoo.com.sg>
2016-03-05 13:11:12 -0600

    New FTP Archive

---
+++ FTP_Archive/FTP_Archive/FTP.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTP_Archive
+{
+    class FTP
+    {
+        private string _FTP_Server;
+        private string _FTP_User;
+        private string _FTP_Password;
+
+        public FTP(string FTP_Server, string FTP_User, string FTP_Password)
+        {
+            _FTP_Server = FTP_Server;
+            _FTP_User = FTP_User;
+            _FTP_Password = FTP_Password;
+        }
+
+        public List<string> Get_List()
+        {
+            List<string> result = new List<string>();
+
+            FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(_FTP_Server);
+            ftpRequest.Credentials = new NetworkCredential(_FTP_User, _FTP_Password);
+            ftpRequest.Method = WebRequestMethods.Ftp.ListDirectory;
+            FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse();
+
+            StreamReader streamReader = new StreamReader(response.GetResponseStream());
+
+            List<string> contents = new List<string>();
+
+            string line = streamReader.ReadLine();
+            while (!string.IsNullOrEmpty(line))
+            {
+                contents.Add(line);
+                line = streamReader.ReadLine();
+            }
+
+            response.Close();
+            streamReader.Close();
+
+            for (int i = 0; i <= contents.Count - 1; i++)
+            {
+                if (contents[i].Contains("."))
+                {
+                    result.Add(_FTP_Server + contents[i]);
+                }
+                else
+                {
+                    //its a folder
+                    FTP list = new FTP(_FTP_Server + contents[i] + "/", _FTP_User, _FTP_Password);
+                    result.AddRange(list.Get_List());                }
+            }
+
+            return result;
+        }
+
+        public void Download(string file, string destination)
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(destination));
+            WebClient ftpClient = new WebClient();
+            ftpClient.Credentials = new System.Net.NetworkCredential(_FTP_User, _FTP_Password);
+            ftpClient.DownloadFile(file, destination);
+        }
+
+        public void Delete(string file)
+        {
+            FtpWebRequest ftpRequest = (FtpWebRequest)WebRequest.Create(file);
+            ftpRequest.Credentials = new NetworkCredential(_FTP_User, _FTP_Password);
+            ftpRequest.Method = WebRequestMethods.Ftp.DeleteFile;
+            FtpWebResponse response = (FtpWebResponse)ftpRequest.GetResponse();
+        }      
+    }
+}
---
+++ FTP_Archive/FTP_Archive/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Configuration;
+
+namespace FTP_Archive
+{
+    class Program
+    {
+        static string FTP_Server = "";
+        static string FTP_User = "";
+        static string FTP_Password = "";
+        static string Archive_Dir = "";
+        static string Archive_Date = "";
+        static string Temp_Dir = "";
+
+        static List<string> files;
+
+        static void Main(string[] args)
+        {
+            files = new List<string>();
+
+            FTP_Server = ConfigurationManager.AppSettings["FTP_Server"];
+            FTP_User = ConfigurationManager.AppSettings["FTP_User"];
+            FTP_Password = ConfigurationManager.AppSettings["FTP_Password"];
+            Archive_Dir = ConfigurationManager.AppSettings["Archive_Dir"];
+            Archive_Date = ConfigurationManager.AppSettings["Archive_Date"];
+            Temp_Dir = ConfigurationManager.AppSettings["Temp_Dir"];
+
+            Directory.CreateDirectory(Archive_Dir + Temp_Dir);
+
+            //get list of files at ftp
+            FTP ftp = new FTP(FTP_Server, FTP_User, FTP_Password);
+            files = ftp.Get_List();
+
+
+            //download and delete
+            for (int i = 0; i <= files.Count - 1; i++)
+            {
+                files[i] = files[i].Replace(FTP_Server, "/");
+                if (files[i].Contains("."))
+                {
+                    string source = FTP_Server + files[i].ToString();
+                    string destination = (Archive_Dir + Temp_Dir + files[i].ToString()).Replace("/", "\\");
+                    ftp.Download(source, destination);
+                    ftp.Delete(source);
+                    //Console.WriteLine(files[i].ToString());
+                }
+            }
+
+            //compress into zip
+            ZipFile.CreateFromDirectory(Archive_Dir + Temp_Dir, Archive_Dir + DateTime.Now.ToString(Archive_Date) + ".zip");
+
+            //delete files on ftp
+            Directory.Delete(Archive_Dir + Temp_Dir, true);
+        }
+    }
+}
