b8f2bb843a7cd0c395015d6b7195b5251e789f9d
den85nis <den85nis@yahoo.com.sg>
2016-03-06 17:59:18 -0600

    Update

--- FTP_Archive/FTP_Archive/Program.cs
+++ FTP_Archive/FTP_Archive/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Configuration;
+using System.Linq;
 
 namespace FTP_Archive
 {
@@ -11,9 +12,12 @@ namespace FTP_Archive
         static string FTP_Server = "";
         static string FTP_User = "";
         static string FTP_Password = "";
-        static string Archive_Dir = "";
-        static string Archive_Date = "";
+        static string Archive_Temporary_Dir = "";
+        static string Archive_Permanent_Dir = "";
         static string Temp_Dir = "";
+        static string Archive_File_Format = "";
+        static int Archive_Temporary_Count = 0;
+        static string Archive_Permanent_Match_Patten = "";
 
         static List<string> files;
 
@@ -24,16 +28,20 @@ namespace FTP_Archive
             FTP_Server = ConfigurationManager.AppSettings["FTP_Server"];
             FTP_User = ConfigurationManager.AppSettings["FTP_User"];
             FTP_Password = ConfigurationManager.AppSettings["FTP_Password"];
-            Archive_Dir = ConfigurationManager.AppSettings["Archive_Dir"];
-            Archive_Date = ConfigurationManager.AppSettings["Archive_Date"];
+            Archive_Temporary_Dir = ConfigurationManager.AppSettings["Archive_Temporary_Dir"];
+            Archive_Permanent_Dir = ConfigurationManager.AppSettings["Archive_Permanent_Dir"];
             Temp_Dir = ConfigurationManager.AppSettings["Temp_Dir"];
+            Archive_File_Format = ConfigurationManager.AppSettings["Archive_File_Format"];
+            Archive_Temporary_Count = Convert.ToInt32(ConfigurationManager.AppSettings["Archive_Temporary_Count"]);
+            Archive_Permanent_Match_Patten = ConfigurationManager.AppSettings["Archive_Permanent_Match_Patten"];
 
-            Directory.CreateDirectory(Archive_Dir + Temp_Dir);
+            Directory.CreateDirectory(Archive_Temporary_Dir);
+            Directory.CreateDirectory(Archive_Permanent_Dir);            
+            Directory.CreateDirectory(Temp_Dir);
 
             //get list of files at ftp
             FTP ftp = new FTP(FTP_Server, FTP_User, FTP_Password);
             files = ftp.Get_List();
-
 
             //download and delete
             for (int i = 0; i <= files.Count - 1; i++)
@@ -42,18 +50,37 @@ namespace FTP_Archive
                 if (files[i].Contains("."))
                 {
                     string source = FTP_Server + files[i].ToString();
-                    string destination = (Archive_Dir + Temp_Dir + files[i].ToString()).Replace("/", "\\");
+                    string destination = (Temp_Dir + files[i].ToString()).Replace("/", "\\");
                     ftp.Download(source, destination);
                     ftp.Delete(source);
                     //Console.WriteLine(files[i].ToString());
                 }
             }
 
+            string Filename = DateTime.Now.ToString(Archive_File_Format) + ".zip";
             //compress into zip
-            ZipFile.CreateFromDirectory(Archive_Dir + Temp_Dir, Archive_Dir + DateTime.Now.ToString(Archive_Date) + ".zip");
+            ZipFile.CreateFromDirectory(Temp_Dir, Archive_Temporary_Dir + Filename);
 
-            //delete files on ftp
-            Directory.Delete(Archive_Dir + Temp_Dir, true);
+            //copy to Long Dir if matches
+            if (Filename.Contains(Archive_Permanent_Match_Patten))
+            {
+                File.Copy(Archive_Temporary_Dir + Filename, Archive_Permanent_Dir + DateTime.Now.ToString(Archive_File_Format) + ".zip");
+            }
+
+            //delete old files in Archive_Temporary_Dir
+            var tempfiles = Directory.GetFiles(Archive_Temporary_Dir, "*.zip").OrderByDescending(d => new FileInfo(d).CreationTime);
+            int j = 1;
+            foreach (string file in tempfiles)
+            {
+                if (j > Archive_Temporary_Count)
+                {
+                    File.Delete(file);
+                }
+                j++;
+            }
+
+            //delete temp
+            Directory.Delete(Temp_Dir, true);
         }
     }
 }
